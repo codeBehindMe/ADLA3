0930504432c27f3ae76d7f12d7ec5f6aa593a1cf
Aaron Tillekeratne <Aaron Tillekeratne>
2016-11-24 20:55:08 +1100

    started adaBoostedTree

--- Aaron/10001_Script.R
+++ Aaron/10001_Script.R
@@ -1,18 +1,17 @@
 ## Data Visualisation.
 require(ggplot2)
 require(scales)
-require(GGally)
+require(caret)
 
-# setwd("C:/Users/aaron/OneDrive/Documents/Monash Data Science/Applied Data Analysis/A3/")
+
+setwd("C:/Users/aaron/OneDrive/Documents/Monash Data Science/Applied Data Analysis/A3/Aaron")
 source("../utilities.r")
-dt_ <- read.csv("prostate.csv")
+dt_ <- read.csv("../prostate.csv")
 
 
 
 str(dt_)
 summary(dt_)
-
-GGally::ggpairs(dt_[,-1])
 
 
 ggplot(dt_, aes(dt_$Result)) + geom_bar() + ggtitle("Result Feature Class Observation Density") + xlab("Result") + ylab("Observation Count")
@@ -20,15 +19,43 @@ ggplot(dt_, aes(dt_$Result)) + geom_bar() + ggtitle("Result Feature Class Observ
 ## Classes 3 & 4 are misrepresented. Especially class 4.
 # This prettey much means that simple models are straight away out since they will learn the bias to mispredict low occurance classes.
 
-## Jitters
-ggplot(dt_, aes(as.factor(dt_$ATT1), as.factor(dt_$ATT2), col = as.factor(dt_$Result))) + geom_jitter() + xlab("ATT1") + ylab("ATT2") + ggtitle("Jitter Results ATT1 ~ ATT2 ")
 
 ## PCA
 dt_pca <- Udf.Utilities.Prcomp(dt_[, c(-1, - ncol(dt_))])$components
 
 ggplot(dt_pca,aes(dt_pca$PC1,dt_pca$PC2,col=as.factor(dt_$Result))) + geom_point() + xlab("PC1") + ylab("PC2") + ggtitle("Class Distribution on Principle Components") + scale_color_discrete(name = "Result")
 
-
+## Jitters
 Udf.Utilities.Jitter(dt_,"ATT1","ATT2","Result")
-Udf.Utilities.Jitter(dt_,"ATT2","ATT3","Result")
+Udf.Utilities.Jitter(dt_, "ATT2", "ATT3", "Result")
+Udf.Utilities.Jitter(dt_, "ATT3", "ATT4", "Result")
+
+
+
+# Resampling inline.
+
+self.adaBoostedTree <- function(Dataframe, target) {
+
+    warning("Application specific function call. May return incorrect values if used out of context.")
+
+    require(caret)
+    require(adabag)
+    require(plyr)
+
+
+    df_ <- Dataframe
+
+    ctrl_ <- trainControl(method = "cv", number = 10, sampling = "down")
+
+
+    dt_[, target] <- as.factor(dt_[, target])
+
+    mdl_ <- train(target ~ . ,method = 'AdaBoost.M1')
+
+
+
+
+}
+
+
 
--- utilities.r
+++ utilities.r
@@ -78,7 +78,7 @@ Udf.Utilities.Jitter <- function(Dataframe,x,y,colBy=NULL){
     ### Wrapper function for ggplot.jitterplot()
     require(ggplot2)
 
-    dt_ <- Dataframe # Local Instance
+    dt_ <- Dataframe 
     plotObj_ <- ggplot(dt_,aes(as.factor(dt_[,x]),as.factor(dt_[,y]))) # Generate ggplot object.
 
     if(!is.null(colBy)){ ## Assess the color scheme requirements. 
@@ -97,6 +97,7 @@ Udf.Utilities.Jitter <- function(Dataframe,x,y,colBy=NULL){
 
 Udf.Utilities.Comboplot <- function(Dataframe,FUN,...) {
 
+    stop("Not Implemented")
     # Generates objects from custom function calls on a dataframe columns.
     require(ggplot2)
 
@@ -111,11 +112,43 @@ Udf.Utilities.Comboplot <- function(Dataframe,FUN,...) {
 
 
     }
+}
+
+Udf.Utilities.Scatter <- function(Dataframe,x,y,colBy=NULL){
+    stop("Not Implemented")
+    require(ggplot2)
+    require(gridExtra)
+
+    dt_ <- Dataframe 
+
+    hist_top <- ggplot() + geom_histogram(aes(dt_[,x]))
+
+    empty <- ggplot() + geom_point(aes(1,1),colour = "white") + theme(
+        axis.ticks = element_blank(),
+        panel.background = element_blank(),
+        axis.text.x = element_blank(), axis.text.y = element_blank(),
+        axis.title.x = element_blank(), axis.title.y = element_blank()
+    )
+
+    scatter <- ggplot() + geom_point(aes(dt_[,x],dt_[,y]))
+
+    hist_right <- ggplot() + geom_histogram(aes(dt_[,y])) + coord_flip()
+
+    gaObject <- grid.arrange(hist_top,empty,scatter,hist_right,ncol = 2, nrow = 2, widths = c(4,1), heights = c(1,4))
+
+    return(gaObject)
+}
 
 
 
+Udf.Utilities.DownSample <- function(Dataframe,target){
+    # Returns down sampled dataframe.
+    
+    # Check dataframe
+    if(class(Dataframe) != "data.frame") stop(paste0("Don't know how to handle object of type ",class(Dataframe)))
 
+    dt_ <- Dataframe 
 
-
+    fLev <- levels(as.factor(Dataframe[,target]))
 
 }
